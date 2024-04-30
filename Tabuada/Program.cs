4fd8e281be37a204ef1a0172f3f453a9979c92fc
vinicius.gfranco2 <vinicius.gfranco2@SMP0581417W10-1>
2024-04-29 22:09:56 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,12 @@
+﻿int numTabuada;
+
+Console.Write("Digite o número da tabuada desejada:");
+numTabuada = Convert.ToInt32(Console.ReadLine());
+
+//repetição 10x
+
+for (int i = 1; i <=10; i++)
+{
+    //interpolação de string $
+    Console.WriteLine($"{numTabuada}x{i} = {numTabuada * i}");
+}
